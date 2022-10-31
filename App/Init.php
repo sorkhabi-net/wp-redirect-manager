859816b3cca3572d258bb2642d385ea2b2dc5c08
Sorkhabi.NeT <sorkhabi.net@gmail.com>
2022-10-31 21:02:29 +0330

    Plugin Initializer Class

---
+++ App/Init.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @package SD_WPRedirectManager
+ */
+
+ namespace SDWPRM;
+
+ final class Init 
+ {
+    public static function get_processes ()
+    {
+        return [
+            Base\Enqueue::class,
+        ];
+    }
+
+    public static function instantiate ($class)
+    {
+        return new $class ();
+    }
+
+    public static function run ()
+    {
+        foreach (self::get_processes() as $class){
+            $process = self::instantiate ($class);
+            if (method_exists($process, 'run')){
+                $process->run ();
+            }
+        }
+    }
+ }
