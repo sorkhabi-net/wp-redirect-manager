dec8cb61ab21ac0f003a7015346a4021f635dc24
Sorkhabi.NeT <sorkhabi.net@gmail.com>
2022-10-31 17:35:56 +0330

    Initial Commit

---
+++ wp-redirect-manager.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package SD_WPRedirectManager
+ */
+
+/*
+Plugin Name: WP Redirect Manager
+Plugin URI: https://github.com/sorkhabi-net/wp-redirect-manager/
+Description: Wordpress Easy and Powerful redirect manager plugin.
+Author: Sorkh Dev "Sorkhabi.NeT"
+Author URI: https://sorkhabi.net/
+Version: 0.0.1
+*/
