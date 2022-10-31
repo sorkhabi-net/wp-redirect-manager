62353d8cc75453eaabcd183645b20d3d36b0e9ef
Sorkhabi.NeT <sorkhabi.net@gmail.com>
2022-10-31 21:05:59 +0330

    Load Inital Class and activate/deactivate hook

--- wp-redirect-manager.php
+++ wp-redirect-manager.php
@@ -16,7 +16,26 @@ License: GPLv2 or later
 defined ('ABSPATH') or die ('Access denied!');
 
 $autoload = dirname (__FILE__) . '/vendor/autoload.php';
-
 if (file_exists ($autoload)){
     require_once $autoload;
+}
+
+function activate_sdwprm (){
+    if (class_exists('SDWPRM\\Base\\Activate')) {
+        SDWPRM\Base\Activate::run();
+    }
+}
+
+function deactivate_sdwprm (){
+    if (class_exists('SDWPRM\\Base\\Deactivate')) {
+        SDWPRM\Base\Deactivate::run();
+    }
+}
+
+register_activation_hook (__FILE__, 'activate_sdwprm');
+register_deactivation_hook (__FILE__, 'deactivate_sdwprm');
+
+
+if (class_exists('SDWPRM\\Init')){
+    SDWPRM\Init::run ();
 }
