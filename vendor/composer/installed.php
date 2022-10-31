510591b17c319ab70fc73475fb434cf8fd1ed497
Sorkhabi.NeT <sorkhabi.net@gmail.com>
2022-10-31 19:38:20 +0330

    Install and config composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'sorkhabi-net/wp-redirect-manager',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '761014e1dd3948a6529f520a6395c6296f078bcf',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'sorkhabi-net/wp-redirect-manager' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '761014e1dd3948a6529f520a6395c6296f078bcf',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
