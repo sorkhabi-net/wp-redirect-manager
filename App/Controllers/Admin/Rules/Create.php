ef247a28dd17da37fba197d84a91d7f4d93a8e4b
Sorkhabi.NeT <sorkhabi.net@gmail.com>
2022-11-27 01:35:44 +0330

    Create new redirect rule option

--- App/Controllers/Admin/Rules/Create.php
+++ App/Controllers/Admin/Rules/Create.php
@@ -7,9 +7,58 @@
 namespace App\Controllers\Admin\Rules;
 
 use App\Base\Controller;
+use App\Base\Helper;
 
 class Create extends Controller
 {
+    public function post()
+    {
+        global $wpdb;
+        // CSRF Check
+        if (!(isset($_POST['form_nonce']) && wp_verify_nonce($_POST['form_nonce'], $this->plugin_slug . 'create_rule'))) {
+            _e('Access diened');
+            return;
+        }
+        if (!isset($_POST['uri']) or !isset($_POST['redirect_to']) or !isset($_POST['status'])) {
+            _e('Please enter all required inputs.');
+            return;
+        }
+        $uri = trim($_POST['uri']);
+        $uri_hash = Helper::hash($uri);
+        $redirect_to = $_POST['redirect_to'];
+        $status = $_POST['status'] == 1 ? 1 : 0;
+        if (mb_strlen($uri, 'UTF-8') == 0 or mb_strlen($uri, 'UTF-8') > 255) {
+            _e('Redirect from must be 1 char and under 255 char');
+            return;
+        }
+        if (mb_strlen($redirect_to, 'UTF-8') == 0 or mb_strlen($redirect_to, 'UTF-8') > 255) {
+            _e('Redirect to must be 1 char and under 255 char');
+            return;
+        }
+        $rule = $wpdb->get_row("SELECT * FROM `{$this->rules_table_name}` WHERE `uri_hash`='{$uri_hash}' LIMIT 1");
+        if ($rule !== null) {
+            _e('Redirect rule is duplicate > edit #' . $rule->id);
+            return;
+        }
+        $result = $wpdb->insert(
+            $this->rules_table_name,
+            [
+                'uri' => $uri,
+                'uri_hash' => $uri_hash,
+                'redirect_to' => $redirect_to,
+                'status' => $status,
+            ],
+            ['%s', '%s', '%s', '%d']
+        );
+        if ($result) {
+            wp_redirect($this->route('rules'));
+            exit();
+        } else {
+            _e('Error while adding data.');
+            return;
+        }
+    }
+
     public function index()
     {
         $this->admin_view('rules.create');
--- App/Views/admin/rules/create.php
+++ App/Views/admin/rules/create.php
@@ -1,3 +1,29 @@
 <?php defined('ABSPATH') or die('Access denied!'); ?>
-<h1>Add new Rule</h1>
-<?php
+<div class="wrap">
+    <h1>Add new redirect rule</h1>
+    <form action="<?php echo $this->route('rules.create'); ?>" method="POST">
+        <input name="form_nonce" type="hidden" value="<?= wp_create_nonce($this->plugin_slug . 'create_rule') ?>" />
+        <table class="form-table" role="presentation">
+            <tr>
+                <th scope="row"><label for="uri">Redirect from:</label></th>
+                <td><?php echo site_url(); ?>/<input name="uri" type="text" id="uri" value="" class="regular-text" required="required"/></td>
+            </tr>
+            <tr>
+                <th scope="row"><label for="redirect_to">Redirect to:</label></th>
+                <td><input name="redirect_to" type="text" id="redirect_to" value="" class="regular-text" placeholder="https://site.com/" required="required"/></td>
+            </tr>
+            <tr>
+                <th scope="row"><label for="status">Status:</label></th>
+                <td>
+                    <select name="status" id="status">
+                        <option value="1">Active</option>
+                        <option value="0">Deactive</option>
+                    </select>
+                </td>
+            </tr>
+        </table>
+        <p class="submit">
+            <button type="submit" class="button button-primary">Add</button>
+        </p>
+    </form>
+</div>
