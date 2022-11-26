4dd0a0ca693b5db639a4f9adf330bc91fd5a2b25
Sorkhabi.NeT <sorkhabi.net@gmail.com>
2022-11-27 02:19:34 +0330

    indent

--- wp-redirect-manager.php
+++ wp-redirect-manager.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @package SDWPRM
  */
@@ -14,30 +15,30 @@ Version: 0.4.3
 License: GPLv2 or later
 */
 
-defined ('ABSPATH') or die ('Access denied!');
-define ('SDWPRM_BASE_FILE', plugin_basename(__FILE__));
+defined('ABSPATH') or die('Access denied!');
+define('SDWPRM_BASE_FILE', plugin_basename(__FILE__));
 
 // Autoload
-$autoload = dirname (__FILE__) . '/vendor/autoload.php';
-if (file_exists ($autoload)){
+$autoload = dirname(__FILE__) . '/vendor/autoload.php';
+if (file_exists($autoload)) {
     require_once $autoload;
 }
 
 // Activate Plugin
-register_activation_hook (__FILE__, function (){
+register_activation_hook(__FILE__, function () {
     if (class_exists('App\\Base\\Activate')) {
         (new App\Base\Activate)->run();
     }
 });
 
 // Deactivate Plugin
-register_deactivation_hook (__FILE__, function (){
+register_deactivation_hook(__FILE__, function () {
     if (class_exists('App\\Base\\Deactivate')) {
         (new App\Base\Deactivate)->run();
     }
 });
 
 // Initialize Plugin
-if (class_exists('App\\App')){
-    App\App::run ();
+if (class_exists('App\\App')) {
+    App\App::run();
 }
