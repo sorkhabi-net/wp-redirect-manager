f7b9f37179998db40967db6ab82ce0a9aa0316b2
Sorkhabi.NeT <sorkhabi.net@gmail.com>
2022-11-26 21:33:39 +0330

    Create access denied error view

---
+++ App/Views/system/errors/access_denied.php
@@ -0,0 +1,6 @@
+<?php defined('ABSPATH') or die('Access denied!'); ?>
+<div class="wrap">
+    <div class="notice notice-danger is-dismissible">
+        <p><?php _e('Access denied!', 'SDWPRM'); ?></p>
+    </div>
+</div>
