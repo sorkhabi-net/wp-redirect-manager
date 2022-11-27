314d0156435a5de4075d09a79389853b60612d09
Sorkhabi.NeT <sorkhabi.net@gmail.com>
2022-11-27 03:33:01 +0330

    Add rule_is_not_exists Notice

--- App/Base/Notice.php
+++ App/Base/Notice.php
@@ -12,11 +12,15 @@ class Notice
     {
         return [
             'rule_deleted_successfully' => [
-                'message' => __ ('Redirect rule deleted successfully.', 'SDWPRM'),
+                'message' => __('Redirect rule deleted successfully.', 'SDWPRM'),
                 'type' => 'success',
             ],
             'try_again' => [
-                'message' => __ ('Something wrong! please try again.', 'SDWPRM'),
+                'message' => __('Something wrong! please try again.', 'SDWPRM'),
+                'type' => 'error',
+            ],
+            'rule_is_not_exists' => [
+                'message' => __('Redirect rule is not exists. <a href="#" onclick="window.history.go(-1); return false;"><strong>Back</strong></a>', 'SDWPRM'),
                 'type' => 'error',
             ],
         ];
@@ -28,13 +32,13 @@ class Notice
         <p>' . $notice['message'] . '</p>
         </div>';
     }
-    public static function show()
+    public static function show($notice_key = null)
     {
-        if (isset($_GET['notice'])) {
+        if ($notice_key === null and isset($_GET['notice'])) {
             $notice_key = $_GET['notice'];
-            if (isset(self::notices()[$notice_key])) {
-                echo self::render(self::notices()[$notice_key]);
-            }
+        }
+        if (isset(self::notices()[$notice_key])) {
+            echo self::render(self::notices()[$notice_key]);
         }
     }
 }
