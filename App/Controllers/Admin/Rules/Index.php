693586d9d46c042a07ac0d0bb961a0228a467350
Sorkhabi.NeT <sorkhabi.net@gmail.com>
2022-11-27 03:36:10 +0330

    show 10 result in page

--- App/Controllers/Admin/Rules/Index.php
+++ App/Controllers/Admin/Rules/Index.php
@@ -13,7 +13,7 @@ class Index extends Controller
     public function index()
     {
         global $wpdb;
-        $rules = $wpdb->get_results("SELECT * FROM `{$this->rules_table_name}` ORDER BY `id` DESC LIMIT 3");
+        $rules = $wpdb->get_results("SELECT * FROM `{$this->rules_table_name}` ORDER BY `id` DESC LIMIT 10");
         $this->admin_view('rules.index', compact('rules'));
     }
 }
