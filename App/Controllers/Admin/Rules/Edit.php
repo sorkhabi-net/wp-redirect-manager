66a20da4b206e9d2750ee0a8c80a8222be9ead78
Sorkhabi.NeT <sorkhabi.net@gmail.com>
2022-11-27 03:54:09 +0330

    Edit redirect rule option

--- App/Controllers/Admin/Rules/Edit.php
+++ App/Controllers/Admin/Rules/Edit.php
@@ -7,12 +7,88 @@
 namespace App\Controllers\Admin\Rules;
 
 use App\Base\Controller;
+use App\Base\Helper;
+use App\Base\Notice;
 
 class Edit extends Controller
 {
+    public function post()
+    {
+        global $wpdb;
+        $id = $_GET['id'] ?? 0;
+        // CSRF Check
+        if (!(isset($_POST['form_nonce']) && wp_verify_nonce($_POST['form_nonce'], $this->plugin_slug . 'edit_rule'))) {
+            _e('Access diened');
+            return;
+        }
+        if (!isset($_POST['uri']) or !isset($_POST['redirect_to']) or !isset($_POST['status'])) {
+            _e('Please enter all required inputs.');
+            return;
+        }
+        $rule = $wpdb->get_row("SELECT * FROM `{$this->rules_table_name}` WHERE `id`='{$id}' LIMIT 1");
+        if ($rule === null) {
+            _e('Redirect rule is not exists.');
+            return;
+        }
+        $uri = trim($_POST['uri']);
+        $uri_hash = Helper::hash($uri);
+        $redirect_to = $_POST['redirect_to'];
+        $status = $_POST['status'] == 1 ? 1 : 0;
+        if (mb_strlen($uri, 'UTF-8') == 0 or mb_strlen($uri, 'UTF-8') > 255) {
+            _e('Redirect from must be 1 char and under 255 char');
+            return;
+        }
+        if (mb_strlen($redirect_to, 'UTF-8') == 0 or mb_strlen($redirect_to, 'UTF-8') > 255) {
+            _e('Redirect to must be 1 char and under 255 char');
+            return;
+        }
+        $other_rule = $wpdb->get_row("SELECT * FROM `{$this->rules_table_name}` WHERE `uri_hash`='{$uri_hash}' and `id`!='{$id}' LIMIT 1");
+        if ($other_rule !== null) {
+            _e('Redirect rule is duplicate > edit #' . $other_rule->id);
+            return;
+        }
+        echo '<pre>';
+        var_dump(
+            $this->rules_table_name,
+            [
+                'uri' => $uri,
+                'uri_hash' => $uri_hash,
+                'redirect_to' => $redirect_to,
+                'status' => $status,
+            ],
+            [
+                'id' => $rule->id,
+            ],
+            ['%s', '%s', '%s', '%d'],
+            ['%d']
+        );
+        $result = $wpdb->update(
+            $this->rules_table_name,
+            [
+                'uri' => $uri,
+                'uri_hash' => $uri_hash,
+                'redirect_to' => $redirect_to,
+                'status' => $status,
+            ],
+            [
+                'id' => $rule->id,
+            ],
+            ['%s', '%s', '%s', '%d']
+            ,
+            ['%d']
+        );
+        wp_redirect($this->route('rules', ['notice' => 'rule_updated_successfully']));
+    }
+
     public function index()
     {
+        global $wpdb;
         $id = $_GET ['id'] ?? 0;
-        $this->admin_view('rules.edit', compact ('id'));
+        $rule = $wpdb->get_row("SELECT * FROM `{$this->rules_table_name}` WHERE `id`='{$id}' LIMIT 1");
+        if ($rule !== null) {
+            $this->admin_view('rules.edit', compact ('rule'));
+        }else{
+            return Notice::show('rule_is_not_exists');
+        }
     }
 }
--- App/Views/admin/rules/edit.php
+++ App/Views/admin/rules/edit.php
@@ -1,3 +1,34 @@
-<?php defined('ABSPATH') or die('Access denied!'); ?>
-<h1>Edit Rule <?php echo $id; ?></h1>
 <?php
+
+use App\Base\Notice;
+
+defined('ABSPATH') or die('Access denied!'); ?>
+<div class="wrap">
+    <?php Notice::show(); ?>
+    <h1>Edit redirect rule #<?php echo $rule->id; ?></h1>
+    <form action="<?php echo $this->route('rules.edit', ['id' => $rule->id]); ?>" method="POST">
+        <input name="form_nonce" type="hidden" value="<?= wp_create_nonce($this->plugin_slug . 'edit_rule') ?>" />
+        <table class="form-table" role="presentation">
+            <tr>
+                <th scope="row"><label for="uri">Redirect from:</label></th>
+                <td><?php echo site_url(); ?>/<input name="uri" type="text" id="uri" value="<?php echo $rule->uri; ?>" class="regular-text" required="required" /></td>
+            </tr>
+            <tr>
+                <th scope="row"><label for="redirect_to">Redirect to:</label></th>
+                <td><input name="redirect_to" type="text" id="redirect_to" value="<?php echo $rule->redirect_to; ?>" class="regular-text" placeholder="https://site.com/" required="required" /></td>
+            </tr>
+            <tr>
+                <th scope="row"><label for="status">Status:</label></th>
+                <td>
+                    <select name="status" id="status">
+                        <option value="0">Deactive</option>
+                        <option value="1"<?php echo ($rule->status ? ' selected="selected"' : ''); ?>>Active</option>
+                    </select>
+                </td>
+            </tr>
+        </table>
+        <p class="submit">
+            <button type="submit" class="button button-primary">Update</button>
+        </p>
+    </form>
+</div>
