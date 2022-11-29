b7458450d37e84ef587f92cd1eb026ef708dcbdd
Sorkhabi.NeT <sorkhabi.net@gmail.com>
2022-11-29 21:44:53 +0330

    Deactive status and error_404 when plugin deactivate

--- App/Base/Deactivate.php
+++ App/Base/Deactivate.php
@@ -10,7 +10,7 @@ class Deactivate extends Controller
 {
     public function run()
     {
-        $this->update_setting('status', 1);
-        $this->update_setting('error_404', 1);
+        $this->update_setting('status', 0);
+        $this->update_setting('error_404', 0);
     }
 }
