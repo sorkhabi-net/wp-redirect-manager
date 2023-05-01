0b7c69a6417159d69e1377a5065b71a5e3a1b3c3
Sorkhabi.NeT <sorkhabi.net@gmail.com>
2023-05-01 23:24:23 +0330

    change version to 0.7.3

--- wp-redirect-manager.php
+++ wp-redirect-manager.php
@@ -11,13 +11,13 @@ Description: Wordpress Easy and Powerful redirect manager plugin.
 Author: Sorkh Dev "Sorkhabi.NeT"
 Author URI: https://sorkhabi.net/
 Text Domain: SDWPRM
-Version: 0.7.2
+Version: 0.7.3
 License: GPLv2 or later
 */
 
 defined('ABSPATH') or die('Access denied!');
 
-define('SDWPRM_PLUGIN_VERSION', '0.7.2');
+define('SDWPRM_PLUGIN_VERSION', '0.7.3');
 define('SDWPRM_BASE_FILE', plugin_basename(__FILE__));
 
 // Autoload
